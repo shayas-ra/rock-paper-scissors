6ec53b497e779bb6f089a96c1eb597a597de7357
Shayas R. Ashraf <shayasra@gmail.com>
2023-09-05 18:03:35 +0300

    Func Created for Player and Computer Choices
    getPlayerChoice Fn Created and assigned to playerSelection
    getComputerChoice Fn Created and assigned to computerSelection

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
 console.log('Rock, Paper, Scissors');
+
+function getComputerChoice () {
+    let randomNumber = Math.floor(Math.random() * 3);
+    let choice = ['ROCK', 'PAPER', 'SCISSORS'];
+    return choice[randomNumber];
+}
+
+function getPlayerChoice () {
+    let choice = prompt('ROCK, PAPER, SCISSORS').toLocaleUpperCase();
+    if (choice == 'ROCK' || choice == 'PAPER' || choice == 'SCISSORS') {
+        return choice
+    } else {
+        alert('Enter From the Choices');
+        return getPlayerChoice();
+    }
+}
+
+let computerSelection = getComputerChoice();
+let playerSelection = getPlayerChoice();
