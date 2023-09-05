0df0a97c8799e1d8ba554c03e6e292a956b0b3e2
Shayas R. Ashraf <shayasra@gmail.com>
2023-09-05 21:23:59 +0300

    Func playRound and game is Created
    Func playRound is To play and Declare who wins in Each play
    Func game is to iterate playRound

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,53 @@
-console.log('Rock, Paper, Scissors');
+console.log("Rock, Paper, Scissors");
 
-function getComputerChoice () {
-    let randomNumber = Math.floor(Math.random() * 3);
-    let choice = ['ROCK', 'PAPER', 'SCISSORS'];
-    return choice[randomNumber];
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  let choice = ["ROCK", "PAPER", "SCISSORS"];
+  return choice[randomNumber];
 }
 
-function getPlayerChoice () {
-    let choice = prompt('ROCK, PAPER, SCISSORS').toLocaleUpperCase();
-    if (choice == 'ROCK' || choice == 'PAPER' || choice == 'SCISSORS') {
-        return choice
-    } else {
-        alert('Enter From the Choices');
-        return getPlayerChoice();
-    }
+function getPlayerChoice() {
+  let choice = prompt("ROCK, PAPER, SCISSORS").toLocaleUpperCase();
+  if (choice == "ROCK" || choice == "PAPER" || choice == "SCISSORS") {
+    return choice;
+  } else {
+    alert("Enter From the Choices");
+    return getPlayerChoice();
+  }
 }
 
-let computerSelection = getComputerChoice();
-let playerSelection = getPlayerChoice();
+let computerSelection = getComputerChoice;
+let playerSelection = getPlayerChoice;
+
+function playRound(player, computer) {
+  if (player == computer) {
+    return `Player Selected = "${player}" , Computer Selected = "${computer}"
+    It'a a TIE`;
+  } else if (player == "ROCK" && computer == "PAPER") {
+    return `Player Selected = "${player}" , Computer Selected = "${computer}"
+    You Lose! Paper beats Rock`;
+  } else if (player == "ROCK" && computer == "SCISSORS") {
+    return `Player Selected = "${player}" , Computer Selected = "${computer}"
+    You Win! Rock beats Scissors`;
+  } else if (player == "PAPER" && computer == "ROCK") {
+    return `Player Selected = "${player}" , Computer Selected = "${computer}"
+    You Win! Paper beats ROCK`;
+  } else if (player == "PAPER" && computer == "SCISSORS") {
+    return `Player Selected = "${player}" , Computer Selected = "${computer}"
+    You Lose! Scissors beats Paper`;
+  } else if (player == "SCISSORS" && computer == "ROCK") {
+    return `Player Selected = "${player}" , Computer Selected = "${computer}"
+    You Lose! Rock beats Scissors`;
+  } else if (player == "SCISSORS" && computer == "PAPER") {
+    return `Player Selected = "${player}" , Computer Selected = "${computer}"
+    You Win! Scissors beats Paper`;
+  }
+}
+
+function game() {
+  return playRound(playerSelection(), computerSelection());
+}
+
+for (let i = 0; i < 5; i++) {
+  console.log(game());
+}
