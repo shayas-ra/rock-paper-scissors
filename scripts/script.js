030034c645de146911d49fb07b047118dc61102d
Shayas R. Ashraf <shayasra@gmail.com>
2023-09-05 21:48:29 +0300

    Added Score
    Score are added to Player and Computer
    Declare Winner with high Score after 5 Games

--- scripts/script.js
+++ scripts/script.js
@@ -18,28 +18,44 @@ function getPlayerChoice() {
 
 let computerSelection = getComputerChoice;
 let playerSelection = getPlayerChoice;
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(player, computer) {
   if (player == computer) {
-    return `Player Selected = "${player}" , Computer Selected = "${computer}"
+    playerScore += 0;
+    computerScore += 0;
+    return `Player Selected = "${player}" Score = ${playerScore} , Computer Selected = "${computer} Score = ${computerScore}";
     It'a a TIE`;
   } else if (player == "ROCK" && computer == "PAPER") {
-    return `Player Selected = "${player}" , Computer Selected = "${computer}"
+    playerScore += 0;
+    computerScore += 1;
+    return `Player Selected = "${player}" Score = ${playerScore} , Computer Selected = "${computer} Score = ${computerScore}";
     You Lose! Paper beats Rock`;
   } else if (player == "ROCK" && computer == "SCISSORS") {
-    return `Player Selected = "${player}" , Computer Selected = "${computer}"
+    playerScore += 1;
+    computerScore += 0;
+    return `Player Selected = "${player}" Score = ${playerScore} , Computer Selected = "${computer} Score = ${computerScore}";
     You Win! Rock beats Scissors`;
   } else if (player == "PAPER" && computer == "ROCK") {
-    return `Player Selected = "${player}" , Computer Selected = "${computer}"
+    playerScore += 1;
+    computerScore += 0;
+    return `Player Selected = "${player}" Score = ${playerScore} , Computer Selected = "${computer} Score = ${computerScore}";
     You Win! Paper beats ROCK`;
   } else if (player == "PAPER" && computer == "SCISSORS") {
-    return `Player Selected = "${player}" , Computer Selected = "${computer}"
+    playerScore += 0;
+    computerScore += 1;
+    return `Player Selected = "${player}" Score = ${playerScore} , Computer Selected = "${computer} Score = ${computerScore}";
     You Lose! Scissors beats Paper`;
   } else if (player == "SCISSORS" && computer == "ROCK") {
-    return `Player Selected = "${player}" , Computer Selected = "${computer}"
+    playerScore += 0;
+    computerScore += 1;
+    return `Player Selected = "${player}" Score = ${playerScore} , Computer Selected = "${computer} Score = ${computerScore}";
     You Lose! Rock beats Scissors`;
   } else if (player == "SCISSORS" && computer == "PAPER") {
-    return `Player Selected = "${player}" , Computer Selected = "${computer}"
+    playerScore += 1;
+    computerScore += 0;
+    return `Player Selected = "${player}" Score = ${playerScore} , Computer Selected = "${computer} Score = ${computerScore}";
     You Win! Scissors beats Paper`;
   }
 }
@@ -50,4 +66,16 @@ function game() {
 
 for (let i = 0; i < 5; i++) {
   console.log(game());
+}
+
+if (playerScore > computerScore) {
+  console.log(`Player Score is ${playerScore} || Computer Score is ${computerScore}`);
+  console.log(`Player Wins!`);
+}
+else if (playerScore < computerScore) {
+  console.log(`Player Score is ${playerScore} || Computer Score is ${computerScore}`);
+  console.log(`Computer Wins!`)
+} else {
+  console.log(`Player Score is ${playerScore} || Computer Score is ${computerScore}`);
+  console.log("TIE!!!");
 }
